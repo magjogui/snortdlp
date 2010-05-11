fdddcb7a64cb391c92078143f786f5b00c2b293c
saintgosu <saintgosu@6f4b484f-7e2e-9ba7-2289-c08c0bdf422f>
2010-05-11 15:55:24 +0000

    Re-written to object-oriented

--- src/BuildRepositoryHistogram.py
+++ src/BuildRepositoryHistogram.py
@@ -11,12 +11,22 @@ from each file into the global database histogram.
 
 import os
 #import MySQLdb need to install!
-from Histogram import returnHistogram
-
 
 # MySql database interface = MySQLdb
 # http://mysql-python.sourceforge.net/ 
 
+def standardizeText(inputText):
+    
+    """
+    Input: text string to standardize
+    Output: string input text stripped of everything except letters and numbers,
+            each word separated by a single space
+    
+    ToDo: more standardization, i.e. stripping out punctuation etc.
+    
+    """
+    
+    return " ".join(inputText.lower().split())
 
 def addToHistogram(filePath):
     """
@@ -28,13 +38,17 @@ def addToHistogram(filePath):
     ToDo: implement database connection, insert into table, etc.
     """
     
-    #connect to database
-    
     #Read in given file and build a histogram from the text
     inputText = open(filePath,'rb').read()
-    histogram = returnHistogram(inputText)
+    histogram = dict()
     
-    print histogram
+    #split standardized input text into a list of individual words
+    text = standardizeText(inputText).split()
+    
+    # Build the histogram from the input text
+    for word in text: 
+        #update key=word in histogram with the count of that word
+        histogram.update( { word : text.count(word) } ) 
     
     #input histogram into database
 
@@ -51,13 +65,16 @@ def walkLocation(path):
         for file in files:
             addToHistogram(root+file)
         
-def main():
+def crawlLocations(repositoryLocationFile):
     
-    # "locations.txt" contains the repository locations 
-    locations = open("locations.txt", 'r').readlines()
+    """
+    Input: location of file containing repository locations
+    Output: none
+    """
+    
+    # repositoryLocationFile contains the repository locations 
+    locations = open(repositoryLocationFile, 'r').readlines()
     
     for path in locations:
         walkLocation(path)
 
-if __name__ == "__main__":
-    main()
