89d8096a2b57565bb6d305a77536bdbae3fe4c6d
tdean87 <tdean87@6f4b484f-7e2e-9ba7-2289-c08c0bdf422f>
2010-05-11 17:01:27 +0000

    created methods for the RuleCreator class

--- trunk/src/RuleCreator.py
+++ trunk/src/RuleCreator.py
@@ -4,19 +4,36 @@ Created on May 11, 2010
 @author: Will, Tyler
 '''
 
-def addSnortRule(file, string):
+class RuleCreator:
     
-    #open the snort file location for appending
-    snortFile = open(file,'a')
-    sid = 1000000 + len(snortFile.readlines()) + 1
     
-
-def createRegex(string):
-    """
-    Input: string of standardized input to create the regex from
-    Output: regex we can plug into a Snort rule
+    def __init__(self, filename, substring):
+        self.filename = filename
+        self.substring = substring
     
-    ToDo: write the method
-    """
-    
-    return ""
+    def addSnortRule(self):
+        
+        #open the snort file location for appending
+        snortFile = open(file, 'a')
+        self.sid = 1000000 + len(snortFile.readlines()) + 1
+        self.createRule()
+        
+        snortFile.write("\n" + self.rule)
+        
+    def createRule(self):
+        regex = self.createRegex()
+        self.rule = "alert tcp $INTERNAL_NET any -> $EXTERNAL_NET any (msg: DLP" + self.filename + " alert”; pcre:\"" + regex + "\"; sid:" + self.sid + ";)"
+        
+    def createRegex(self):
+        """
+        Input: string of standardized input to create the regex from
+        Output: regex we can plug into a Snort rule
+        
+        ToDo: write the method
+        """
+        
+        words = self.substring.split()
+        rule = ")( )* (".join(words)
+        rule = "/(" + rule + ")/is"
+        
+        return rule
