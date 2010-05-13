0ad21b5ae4f52df92445ab583ac08443135fed45
saintgosu <saintgosu@6f4b484f-7e2e-9ba7-2289-c08c0bdf422f>
2010-05-13 01:16:23 +0000

    Start of translation of Python code to PHP. histogram.php -> implements the histogram methods launcher.php -> basic results page that takes input from basicform.html common.php -> implements many of the common methods such as text standardization basicform.html -> very basic html input form to test regex and rule generation

---
+++ src/common.php
@@ -0,0 +1,63 @@
+<?php
+
+	// Commonly used functions, including the regex and rule generating functions
+	
+	function createSnortRule($sid, $alertName, $inputText){
+		/*
+		 * Given a sid, name and input text, return a Snort rule
+		 * containing a PCRE generated from the input text.
+		 * 
+		 * Note: $alertName should usually be set to the name of the input file
+		 */
+		
+		$regex = createRegex($inputText);
+		$rule = "alert tcp \$HOME_NET any -> \$EXTERNAL_NET any (msg:\"Possible detection of: $alertName\"; pcre:\"$regex\"; classtype:data-loss; sid:$sid;)";
+		
+		return $rule;
+	}
+	
+	function createRegex($inputText){
+		/*
+		 * Create a PCRE regular expression from $inputText. 
+		 */
+		
+		$words = explode(" ", standardizeText($inputText));
+		array_map("sanitizeRegex",$words); //sanitize each word
+		
+		//glue words together and build the regex
+		$regex = "/(" . implode(")( )*(", $words) . ")/is";
+	
+		return $regex;
+	}
+	
+	function sanitizeRegex($inputText){
+		/*
+		 * Escape any reserved regex characters in $inputText.
+		 */
+		
+		$reserved = array("\\","[","^","$",".","|","?","*","+","(",")","{","}");
+		
+        for($i = 0; $i < count($reserved); $i++){
+        	$char = $reserved[$i];
+        	$inputText = str_replace($char, "\\".$char, $inputText);
+        }
+
+		return $inputText;
+	}
+	
+	function standardizeText($inputText){
+		/*
+		 * Standard function to standardize a text string.
+		 * Converts $inputText to lowercase and removes whitespace.
+		 */
+				
+		//convert the string to lowercase and split on whitespace
+		$split = preg_split('/\s+/', strtolower($inputText)); 
+		
+		//reassemble the string, separated by a single space
+		$cleanText = trim(implode(" ", $split));
+		
+		return $cleanText;
+	}
+
+?>
