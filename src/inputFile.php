d6d4114ae03392d9774c2fc5c937bfaaa36394ce
tdean87 <tdean87@6f4b484f-7e2e-9ba7-2289-c08c0bdf422f>
2010-05-25 16:50:41 +0000

    created the display page to display details about a file or folder updated the inputFile page to redirect to itself and take care of the file processing updated the folderPath page to add a link to display

--- src/inputFile.php
+++ src/inputFile.php
@@ -1,4 +1,4 @@
-<?php include("includes/checklogin.php")?>
+<?php include("includes/checklogin.php"); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!--
 Design by Free CSS Templates
@@ -19,18 +19,6 @@ Released   : 20100309
 <meta name="description" content="" />
 <link href="styles/style.css" rel="stylesheet" type="text/css" media="screen" />
 </head>
-
-<script type="text/javascript">
-
-//Quick function that replaces the file name with the name from the input file location box
-function replaceAlertName() { 
-	var path = document.getElementById('inputFile').value;
-	var name = path.split("/");
-	document.getElementById('alertName').value = name[name.length-1];
-}
-
-</script>
-
 <body>
 	<div id="logo">
 		<h1><a href="#">PigPen</a></h1>
@@ -40,29 +28,99 @@ function replaceAlertName() {
 	<!-- end #logo -->
 	<?php include("includes/header.php"); ?>
 	<?php include("includes/common.php"); ?>
+	<?php 
+	
+	/**
+	 * checks if the fileName was past from the post method
+	 */
+	if (isset($_POST['fileName']) && !empty($_POST['fileName']) && isset($_POST['path']) && !empty($_POST['path'])){
+		
+		$fileName = $_POST['fileName'];
+		$path = $_POST['path'];
+		
+	    // Checks to make sure the inputted path is valid and exists
+		if($path{strlen($path)-1} != "/"){
+			$path = $path . "/";
+			if(realpath($path) == false){
+				$path_error = true;
+			}
+		}
+		
+		$completeFile = $path. $fileName;
+		
+		$file = fopen($completeFile, 'r') or die("can't open $completeFile and $");
+		$inputText = fread($file, filesize($completeFile));
+		fclose($file);
+	
+		$config = getConfig();
+		
+		$snortFile = $config['snortFile'];
+		$substringLength = $config['substringLength'];
+		
+		/*
+		 * gets scoring method
+		 */
+		if (isset($_POST['scoringMethod']) && !empty($_POST['scoringMethod'])){
+			$scoringMethod = $_POST['scoringMethod'];
+		}
+		
+		switch($scoringMethod){
+			case "histogram":
+				$substring = selectSubstringHistogram(genHistogram($inputText), $inputText, $substringLength);
+				break;
+			case "modifiedhist":
+				$substring = selectSubstringModifiedHistogram(genHistogram($inputText), $inputText, $substringLength);
+				break;
+			case "multipleRandSamples":
+				$substring = "";
+				break;
+			case "random":
+				$substring = selectSubstringRandom($inputText, $substringLength);
+				break;
+			default:
+				$substring = selectSubstringHistogram(genHistogram($inputText), $inputText, $substringLength);
+		}
+		
+		$regex = createRegex($substring);
+		
+		$rule = createSnortRule(getNextsid($snortFile), $fileName, $substring);
+		
+		writeToFile($snortFile, $rule);
+		
+		include("includes/dbconnect.php");
+		$path = mysql_real_escape_string($path);
+		$fileName = mysql_real_escape_string($fileName);
+		$rule = mysql_real_escape_string($rule);
+		$regex = mysql_real_escape_string($regex);
+		$query = "INSERT INTO rules (file_name, path, rule, regex, type, count) VALUES ('$fileName', '$path', '$rule', '$regex', 1, 1)";
+		mysql_query($query);
+		include("includes/dbclose.php"); 
+	}
+	
+	?>
 	<div id="page">
 		<div id="content">
 		  <div class="post">
 				<h2 class="title">Process New File</a></h2>
 				<div class="entry">
-					<form action="displayResults.php" method="post">
+					<form action="inputFile.php" method="post">
 						<table>
-						<tr><td><b>File: </b><input type="file" id="fileName" name="fileName" value="test.txt"/></td>
-						<td><b>Method: </b>
+						<tr><td><b>Directory: </b><input type="text" id="path" name="path"/></td><td><b>File: </b><input type="text" id="fileName" name="fileName"/></td></tr>
+						<tr><td><b>Method: </b>
 							<SELECT NAME="scoringMethod">
 								<OPTION VALUE=histogram SELECTED>Histogram
 								<OPTION VALUE=modifiedhist>Modified histogram
 								<OPTION VALUE=multipleRandSamples>Multiple random samples
 								<OPTION VALUE=random>Random
-							</SELECT></td></tr>
-							<tr><td><input type="submit" id="create" value="Create" /></td></tr>
+							</SELECT></td>
+							<td align="right"><input type="submit" id="create" value="Create" /></td></tr>
 						</table>						
 					</form>
 					<br><br><br>
 					<h2 class="title">Manage Protected Files</a></h2>
 					<br>
 					<table>
-					<tr><td><b>File</b></td><td colspan="2" align="center"><b>Action</b></td></tr>
+					<tr><td><b>Directory</b><td><b>File</b></td></td><td colspan="3" align="center"><b>Action</b></td></tr>
 					<?php 
 						include("includes/dbconnect.php");
 						
@@ -70,7 +128,7 @@ function replaceAlertName() {
 						$result = mysql_query($query);
 						
 						while($row = mysql_fetch_array($result, MYSQL_ASSOC)){
-							echo "<tr><td width=\"250\">" . $row['path'] . "</td><td>" . $row['file_name'] . "</td><td><a href=\"includes/remove.php?type=file&id=" . $row['rule_id'] . "\">delete</a> |</td><td><a href=#>recalculate</a></td></tr>";
+							echo "<tr><td>" . $row['path'] . "</td><td width=\"250\">" . $row['file_name'] . "</td><td><a href=\"display.php?type=file&id=" . $row['rule_id'] . "\">display</a> |</td><td><a href=\"includes/remove.php?type=file&id=" . $row['rule_id'] . "\">delete</a> |</td><td><a href=#>recalculate</a></td></tr>";
 					
 						}					
 						include("includes/dbclose.php"); 
