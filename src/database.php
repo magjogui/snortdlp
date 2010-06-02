4cb66cfdfcd4b749824fd1fae34e137dba7186eb
tdean87 <tdean87@6f4b484f-7e2e-9ba7-2289-c08c0bdf422f>
2010-06-02 13:38:38 +0000

    added database fingerprinting functionality

--- src/database.php
+++ src/database.php
@@ -26,13 +26,134 @@ Released   : 20100309
 	</div>
 	<hr />
 	<!-- end #logo -->
-	<?php include("includes/header.php"); ?>
+	<?php include("includes/header.php")?>
+	<?php include("includes/common.php")?>
+	<?php 
+	$incomplete = false;
+	if (isset($_POST['server']) && !empty($_POST['server'])){
+		$server = $_POST['server'];
+	}
+	
+	if (isset($_POST['db']) && !empty($_POST['db'])){
+		$db = $_POST['db'];
+	}
+	
+	if (isset($_POST['user']) && !empty($_POST['user'])){
+		$user = $_POST['user'];
+	}
+	
+	if (isset($_POST['pass']) && !empty($_POST['pass'])){
+		$pass = $_POST['pass'];
+	}
+	
+	if (isset($_POST['table']) && !empty($_POST['table'])){
+		$table = $_POST['table'];
+	}
+	
+	if (isset($_POST['port']) && !empty($_POST['port'])){
+		$port = $_POST['port'];
+	}
+	
+	if (isset($_POST['column']) && !empty($_POST['column'])){
+		$column = $_POST['column'];
+	}
+	
+	if ($server !="" && $db !="" && $user !="" && $pass !="" && $table !="" && $column !=""){
+	
+	$dbhost = $server . ":" . $port;
+	$dbuser = $user;
+	$dbpass = $pass;
+	
+	$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die  ('Error connecting to mysql');
+	
+	$dbname = $db;
+	mysql_select_db($dbname);
+	
+	$column = mysql_real_escape_string($column);
+	$table = mysql_real_escape_string($table);
+	
+	$query = "SELECT $column FROM $table";
+	$result = mysql_query($query);
+	
+	$numRows = mysql_num_rows($result); //gets the number of rows from the query
+	
+	$numRowsUsed = (int)sqrt($numRows); //takes the sqare root to get a smaller samlple
+	echo "$numRowsUsed<br>";
+	
+	$regex = "/("; //starts the regex value
+	
+	for($i = 0; $i < $numRowsUsed; $i++){ //loops through the number of number of rows to sample
+		$rowNum = (int)rand($numRowsUsed * $i, $numRowsUsed * ($i + 1)); //selects a random sample from the numer of rows
+		mysql_data_seek($result, $rowNum); //goes to a specific result
+		$row = mysql_fetch_assoc($result); // gets the specific result as a $row
+		
+		$value = $row[$column]; //grabs the random number value
+		$value = sanitizeRegex($value); //escapes any reserved regex char
+		
+		$regex = $regex . $value . "|"; //builds the regex
+	}
+	
+	$regex = substr($regex,0,-1) . ")/i"; //completes the regex
+	
+	
+	mysql_close($conn); //closes the db connection
+	
+	$sid = getNextsid();
+	$rule = "alert tcp \$HOME_NET any -> \$EXTERNAL_NET any (msg:\"Possible detection of: $table : $column\"; pcre:\"$regex\"; classtype:data-loss; sid:$sid;)";
+	
+	include("includes/dbconnect.php");
+	$query = "INSERT INTO rules (file_name, path, rule, regex, count, sid, type) VALUES ('$table', '$column', '$rule', '$regex', 1, $sid, 4)";
+	mysql_query($query);
+	include("includes/dbclose.php");
+	
+	$incomplete = false;
+	
+	} else if(!isset($_POST['server']) && !isset($_POST['server']) && !isset($_POST['table']) && !isset($_POST['db']) && !isset($_POST['port']) && !isset($_POST['user']) && !isset($_POST['pass'])){
+		$incomplete = false;
+	} else {
+		$incomplete = true;
+	}
+	
+	?>
 	<div id="page">
 		<div id="content">
 		  <div class="post">
 				<h2 class="title">Process Database Tables</a></h2>
 				<div class="entry">
-					TODO: create page that allows input for database tables and displays list of current ones (for edit/delete)
+				<?php if($incomplete){
+				echo "<font color=\"red\"><b>One of the fields was incomplete!</b></font><br><br>";
+				}?>
+					<form action="database.php" method="post">
+						<table>
+						<tr><td><b>Server Hostname: </b></td><td><input type="text" id="server" name="server" value="localhost<?php //$_POST['server']?>"/></td></tr>
+						<tr><td><b>Port: </b></td><td><input type="text" id="port" name="port" value="3306<?php //$_POST['port']?>"/></td></tr></tr>
+						<tr><td><b>Database Name: </b></td><td><input type="text" id="db" name="db" value="testdb<?php //$_POST['db']?>"/></td></tr></tr>
+						<tr><td><b>Username: </b></td><td><input type="text" id="user" name="user" value="root<?php //$_POST['user']?>"/></td>
+						<tr><td><b>Password: </b></td><td><input type="password" id="pass" name="pass" value="tartans"/></td></tr>
+						<tr><td><b>Table: </b></td><td><input type="text" id="table" name="table" value="test<?php //$_POST['table']?>"/></td></tr>
+						<tr><td><b>Column: </b></td><td><input type="text" id="column" name="column" value="secret<?php //$_POST['column']?>"/></td></tr>
+						<tr><td align="left"><input type="submit" id="create" value="Create" /></td></tr>
+						</table>						
+					</form>
+					<table>
+					<br>
+					<h2 class="title">Protected Database Tables</a></h2>
+					<br>
+					<tr><td><b>Table</b></td><td><b>Protected Column</b></td><td colspan="3" align="center"><b>Action</b></td></tr>
+					<?php 
+						include("includes/dbconnect.php");
+						
+						$query = "SELECT rule_id, file_name, path FROM rules WHERE type = 4";
+						$result = mysql_query($query);
+						
+						while($row = mysql_fetch_assoc($result)){
+							echo "<tr><td>" . $row['file_name'] ."</td><td>". $row['path'] . "</td><td><a href=\"display.php?type=db&id=" . $row['rule_id'] . "\">display</a> |</td><td><a href=\"includes/remove.php?type=db&id=" . $row['rule_id'] . "\">delete</a> |</td><td><a href=#>recalculate</a></td></tr>";
+						}					
+						include("includes/dbclose.php"); 
+						
+						
+					?>
+					</table>
 			</div>
 		  </div>
 		</div><!-- end #content -->
--- src/includes/remove.php
+++ src/includes/remove.php
@@ -24,10 +24,9 @@
 		include("dbconnect.php");
 		$id = mysql_real_escape_string($id);
 		
-		
-		//TODO: query database for files with type = 2 and path = $id
-		// and remove file --> $query = "DELETE FROM rules WHERE type=2 AND path = $id";
-		
+		//deletes the record for this file
+		$query = "DELETE FROM rules WHERE rule_id = $id";
+		mysql_query($query);
 		
 		include("dbclose.php");
 		header("location: ../folderPath.php");
@@ -45,6 +44,19 @@
 		rewriteRulesFile();
 		
 		header("location: ../freeText.php");
+	} else if($type == "db"){
+		include("dbconnect.php");
+		$id = mysql_real_escape_string($id);
+		
+		//deletes the record for this file
+		$query = "DELETE FROM rules WHERE rule_id = $id";
+		mysql_query($query);
+		include("dbclose.php");
+		
+		//rewrites the rules file with all the rules currently in the db
+		rewriteRulesFile();
+		
+		header("location: ../database.php");
 	}
 	
 ?>
